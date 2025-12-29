a560453fc16b8d6b7d02d01f1283283e6a093938
Javorskinha <andrielijavorski@gmail.com>
2025-12-29 14:37:38 -0300

    revisao do desafio 5 php

--- desafios/005/index.php
+++ desafios/005/index.php
@@ -9,21 +9,23 @@
     <h1>Desafio 005</h1>
     <div>
         <p>Analisador de numeros reais</p>
-        <p>Informe um numero real: </p>
         <form action="index.php" method="post">
+            <label for="numero">Informe um numero real: </label>
+            <br>
             <input type="number" name="numero" id="numero" step="any">
-            <button type="submit">analisar</button>
+            <br>
+            <button type="submit" style="margin-top: 10px;">analisar</button>
         </form>
     </div>
     <?php
         if(isset($_POST['numero'])) {
             $numero = floatval($_POST['numero']);
             $inteiro = intval($numero);
-            $decimal = $numero - $inteiro;
+            $fracao = $numero - $inteiro;
             echo "<ul>
-                    <li>O numero informado foi: <strong>$numero</strong></li>
-                    <li>A parte inteira é: <strong>$inteiro</strong></li>
-                    <li>A parte decimal é: <strong>$decimal</strong></li>
+                    <li>O numero informado foi: <strong>" . number_format($numero, 3, ',', '.') . "</strong></li>
+                    <li>A parte inteira é: <strong>" . number_format($inteiro, 0, ',', '.') . "</strong></li>
+                    <li>A parte fracionária é: <strong>" . number_format($fracao, 3, ',', '.') . "</strong></li>
                 </ul>";
         }
     ?>
